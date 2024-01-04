1518d96c0e2de44dd035b14d2267e8e4a8c9edaf
joelricmisa <joelriccmisa@gmail.com>
2024-01-04 18:08:44 +0800

    Remove image 10, unused

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,4 +1,4 @@
-import { item1, item2, item3, item4, item5, item6, item7, item8, item9, item10 } from "../assets/images";
+import { item1, item2, item3, item4, item5, item6, item7, item8, item9 } from "../assets/images";
 
 export const birthdayData = [
 	{
