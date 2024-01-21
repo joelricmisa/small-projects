b6e58b30ec93fc0b8a83eda0042e716dffc05a3f
joelricmisa <joelriccmisa@gmail.com>
2024-01-21 22:42:12 +0800

    apply react router

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,56 @@ import Menu from "./containers/Menu";
 import Reviews from "./containers/Reviews";
 import Slider from "./containers/Slider";
 import Tours from "./containers/Tours";
+import { createBrowserRouter, createRoutesFromElements, Route, Link, RouterProvider } from "react-router-dom";
+import RootLayout from "./layout/Root";
+import RootIndex from "./containers/RootIndex";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={<RootLayout />}>
+			<Route
+				index
+				element={<RootIndex />}
+			/>
+			<Route
+				path="slider"
+				element={<Slider />}
+			/>
+			<Route
+				path="jobtab"
+				element={<JobTab />}
+			/>
+			<Route
+				path="tours"
+				element={<Tours />}
+			/>
+			<Route
+				path="reviews"
+				element={<Reviews />}
+			/>
+			<Route
+				path="menu"
+				element={<Menu />}
+			/>
+			<Route
+				path="birthdaylist"
+				element={<BirthdayList />}
+			/>
+			<Route
+				path="accordion"
+				element={<Accordion />}
+			/>
+			<Route path="*" />
+		</Route>
+	)
+);
 
 function App() {
 	return (
 		<>
-			<Slider />
-			<JobTab />
-			<Tours />
-			<Reviews />
-			<Menu />
-			<BirthdayList />
-			<Accordion />
+			<RouterProvider router={router} />
 		</>
 	);
 }
