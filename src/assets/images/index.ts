198d1dc15fdc32ecb0f15f74901615b950346b0a
joelricmisa <joelriccmisa@gmail.com>
2024-01-21 22:44:02 +0800

    update the assets index

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -7,5 +7,12 @@ import item6 from "./item-6.jpeg";
 import item7 from "./item-7.jpeg";
 import item8 from "./item-8.jpeg";
 import item9 from "./item-9.jpeg";
+import sliderImg from "./slider.jpg";
+import jobtabImg from "./jobtab.jpg";
+import menuImg from "./menu.jpg";
+import qaImg from "./qa.jpg";
+import reviewsImg from "./reviews.jpg";
+import toursImg from "./tours.jpg";
+import bdImg from "./birthday.jpg";
 
-export { item1, item2, item3, item4, item5, item6, item7, item8, item9 };
+export { item1, item2, item3, item4, item5, item6, item7, item8, item9, sliderImg, jobtabImg, bdImg, menuImg, qaImg, reviewsImg, toursImg };
